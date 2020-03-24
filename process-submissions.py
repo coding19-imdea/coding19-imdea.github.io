599c37e465cb93181d2aea2ae1e2cfb0fd36287b
coding19-imdea <coding19.imdea@gmail.com>
2020-03-24 21:07:21 +0100

    Update index

--- process-submissions.py
+++ process-submissions.py
@@ -33,7 +33,7 @@ if __name__=='__main__':
             exit(1)
         lang = ext2lang[ext]
         if output_type == "nosource":
-            print(f"<li><b>{name.title()}: {lang}</b></li>")
+            print(f"<li><b>{name.title()}: <!--<a target=\"_blank\" href=\"submissions/problem1/{name}.{ext}\">-->{lang}<!--</a>--></b></li>")
         else: # Assume full
             print(f"<li><b>{name.title()}: <a target=\"_blank\" href=\"submissions/problem1/{name}.{ext}\">{lang}</a></b></li>")
 

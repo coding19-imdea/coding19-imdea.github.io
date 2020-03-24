9dd23bfc4fbaef0ffb2841b50758371233d484cc
coding19-imdea <62432732+coding19-imdea@users.noreply.github.com>
2020-03-24 13:06:00 +0100

    Update Aguirre.hs

--- submissions/problem1/Aguirre.hs
+++ submissions/problem1/Aguirre.hs
@@ -1,9 +1,7 @@
 -- ''flips'' ones into ones and underscores
--- if n < 2^m returns [n], otherwise returns
--- [n-2^m, 2n + 1 - , n-2^m]
 
 auxFlip :: Int -> Int -> [Int]
-auxFlip m n = let l = 2^m in
+auxFlip m n = let l = 2^(m+1) in
 		if (n < l) then [n]
 			else [n-l, -(2*l-n) ,n-l]
 
@@ -33,3 +31,11 @@ paintLine (n:ns) = if (n>0) then (concat (replicate n "1")) ++ paintLine ns
 
 computeLine :: Int -> Int -> Int -> String
 computeLine b n t = paintLine $ genLine b n t
+
+
+paintTriangle :: Int -> Int -> IO()
+paintTriangle b t = putStr $ concat [ (computeLine b i t)++"\n" | i <- [0..(2^b -1)] ]
+
+main = do
+  n <- getLine
+  paintTriangle 5 (read n :: Int)

2bb085673da167439da11f5c6955f7334d07b074
coding19-imdea <coding19.imdea@gmail.com>
2020-03-25 13:15:09 +0100

    updated submission file

--- process-submissions.py
+++ process-submissions.py
@@ -1,5 +1,27 @@
 from sys import argv,exit
 from os import listdir
+from bs4 import BeautifulSoup
+import re
+
+def get_cur_submissions_in_order(prob_num):
+    html_doc = open('index.html').read()
+    soup = BeautifulSoup(html_doc, 'html.parser')
+
+    cur_order = []
+
+    try:
+        cnt = soup.find_all(class_="content")[prob_num]
+        items = cnt.find_all("li")
+
+        pattern = f"problem{prob_num}/(.*)\" "
+        for i,x in enumerate(items):
+            # find filename
+            id = re.search(pattern, str(x)).group(1)
+            cur_order.append(id)
+        return cur_order
+    except:
+        return []
+
 
 ext2lang = {
     'ml' : "OCaml",
@@ -17,6 +39,7 @@ ext2lang = {
 }
 
 if __name__=='__main__':
+
     if len(argv) < 3:
         print("Usage: python3 process-submissions.py [PROBLEM_NUMBER] [full|nosource]")
         exit(1)
@@ -24,8 +47,16 @@ if __name__=='__main__':
     prob_num = argv[1]
     output_type = argv[2]
 
-    submissions_raw = listdir(f"submissions/problem{prob_num}")
-    name_ext_lst = [x.split('.') for x in submissions_raw]
+    cur_submissions = get_cur_submissions_in_order(int(prob_num))
+
+    submissions_from_file = listdir(f"submissions/problem{prob_num}")
+
+    part_sorted_submissions = cur_submissions.copy()
+    for subm in submissions_from_file:
+        if subm not in cur_submissions:
+            part_sorted_submissions.append(subm)
+
+    name_ext_lst = [x.split('.') for x in part_sorted_submissions]
 
     for name, ext in name_ext_lst:
         if ext not in ext2lang:

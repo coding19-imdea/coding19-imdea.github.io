fc8eceabfcb01246c1824904089f3742029516b7
coding19-imdea <coding19.imdea@gmail.com>
2020-03-31 15:58:41 +0200

    add response row

--- showSubmissions.js
+++ showSubmissions.js
@@ -85,3 +85,6 @@ submissions[6] = submissions[6].concat('<li>Niki: ' + showcode(6,"https://raw.gi
 if (submissions[7] == undefined) {submissions[7] = "";}
 submissions[7] = submissions[7].concat('<li>Palandjian: ' + showcode(7,"https://raw.githubusercontent.com/coding19-imdea/coding19-imdea.github.io/master/submissions/problem7/Palandjian.c", "C") + '</li>'
 );
+if (submissions[7] == undefined) {submissions[7] = "";}
+submissions[7] = submissions[7].concat('<li>Cesar: ' + showcode(7,"https://raw.githubusercontent.com/coding19-imdea/coding19-imdea.github.io/master/submissions/problem7/Cesar.ml", "ocaml") + '</li>'
+);

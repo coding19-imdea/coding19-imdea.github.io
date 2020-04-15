031ac44913da8a3202317ee34fa237ef69e7075c
coding19-imdea <coding19.imdea@gmail.com>
2020-04-15 22:41:20 +0200

    add response row

--- showSubmissions.js
+++ showSubmissions.js
@@ -223,3 +223,6 @@ submissions[15] = submissions[15].concat('<li>Cesar: ' + showcode(15,"https://ra
 if (submissions[16] == undefined) {submissions[16] = "";}
 submissions[16] = submissions[16].concat('<li>Cesar: ' + showcode(16,"https://raw.githubusercontent.com/coding19-imdea/coding19-imdea.github.io/master/submissions/problem16/Cesar.ml", "ocaml") + '</li>'
 );
+if (submissions[16] == undefined) {submissions[16] = "";}
+submissions[16] = submissions[16].concat('<li>Ignacio Cascudo: ' + showcode(16,"https://raw.githubusercontent.com/coding19-imdea/coding19-imdea.github.io/master/submissions/problem16/Ignacio Cascudo.py", "Python") + '</li>'
+);

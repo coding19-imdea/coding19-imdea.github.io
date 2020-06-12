a7963ab10973fe7d11423da0682d26179383a601
nikivazou <nikivazou@gmail.com>
2020-06-12 12:21:09 +0200

    remove undefineds

--- showSubmissions.js
+++ showSubmissions.js
@@ -303,6 +303,9 @@ submissions[34] = submissions[34].concat('<li>Cesar: ' + showcode(34,"https://ra
 if (submissions[34] == undefined) {submissions[34] = "";}
 submissions[34] = submissions[34].concat('<li>Margarita: ' + showcode(34,"https://raw.githubusercontent.com/coding19-imdea/coding19-imdea.github.io/master/submissions/problem34/Margarita.cpp", "C++") + '</li>'
 );
+submissions[29]="";
+submissions[32]="";
+submissions[35]="";
 if (submissions[36] == undefined) {submissions[36] = "";}
 submissions[36] = submissions[36].concat('<li>Cesar: ' + showcode(36,"https://raw.githubusercontent.com/coding19-imdea/coding19-imdea.github.io/master/submissions/problem36/Cesar.ml", "ocaml") + '</li>'
 );

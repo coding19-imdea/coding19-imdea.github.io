6b4d84e324bbac6ad6f97cece85e2c9d702c9856
coding19-imdea <coding19.imdea@gmail.com>
2021-11-15 12:54:08 +0100

    add src file submissions/problem45/Test.cpp

---
+++ submissions/problem45/Test.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define forr(i,a,b) for(int i=(a); i<(b); i++)
+#define forn(i,n) forr(i,0,n)
+#define zero(v) memset(v, 0, sizeof(v))
+#define forall(it,v) for(auto it=v.begin();it!=v.end();++it)
+#define pb push_back
+#define fst first
+#define snd second
+typedef long long ll;
+typedef pair<ll,ll> pll;
+#define dforn(i,n) for(int i=n-1; i>=0; i--)
+#define sz(x) ((int)((x).size()))
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    return 0;
+}

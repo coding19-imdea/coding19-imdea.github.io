1fd1b9a79b67516160a37b840af09592cc7fce37
coding19-imdea <coding19.imdea@gmail.com>
2021-11-22 20:04:47 +0100

    add src file submissions/problem46/Fedor.go

---
+++ submissions/problem46/Fedor.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"math"
+	"strings"
+	"strconv"
+)
+
+func main() {
+	s := bufio.NewScanner(os.Stdin)
+    for s.Scan() {
+		f := strings.Fields(s.Text())
+		a, _ := strconv.Atoi(f[0])
+		b, _ := strconv.Atoi(f[1])
+		if a == b {
+			fmt.Println(0)
+		} else {
+			fmt.Println(1 << (int32(math.Ilogb(float64(a ^ b))) + 1) - 1)
+		}
+	}
+}

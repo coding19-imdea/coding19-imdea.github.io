c95c1f622ef9c4c400359ce21f166f3568672824
vonaka <vonssung@gmail.com>
2021-11-23 09:08:01 +0100

    Fix formatting.

--- submissions/problem46/Fedor.go
+++ submissions/problem46/Fedor.go
@@ -3,22 +3,22 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os"
 	"math"
-	"strings"
+	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	s := bufio.NewScanner(os.Stdin)
-    for s.Scan() {
+	for s.Scan() {
 		f := strings.Fields(s.Text())
 		a, _ := strconv.Atoi(f[0])
 		b, _ := strconv.Atoi(f[1])
 		if a == b {
 			fmt.Println(0)
 		} else {
-			fmt.Println(1 << (int32(math.Ilogb(float64(a ^ b))) + 1) - 1)
+			fmt.Println(1<<(int32(math.Ilogb(float64(a^b)))+1) - 1)
 		}
 	}
 }

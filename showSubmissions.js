1ee39cc351023fc0f87d34ba8662d0bdca43ab01
coding19-imdea <coding19.imdea@gmail.com>
2021-11-24 10:17:35 +0100

    add response row

--- showSubmissions.js
+++ showSubmissions.js
@@ -359,3 +359,6 @@ submissions[46] = submissions[46].concat('<li>Niki: ' + showcode(46,"https://raw
 if (submissions[46] == undefined) {submissions[46] = "";}
 submissions[46] = submissions[46].concat('<li>Luka: ' + showcode(46,"https://raw.githubusercontent.com/coding19-imdea/coding19-imdea.github.io/master/submissions/problem46/Luka.rkt", "Racket") + '</li>'
 );
+if (submissions[46] == undefined) {submissions[46] = "";}
+submissions[46] = submissions[46].concat('<li>Lydia: ' + showcode(46,"https://raw.githubusercontent.com/coding19-imdea/coding19-imdea.github.io/master/submissions/problem46/Lydia.c", "C") + '</li>'
+);

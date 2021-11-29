422868a2bb1f7440139114cab3bedb3489b64d5a
coding19-imdea <coding19.imdea@gmail.com>
2021-11-29 14:31:22 +0100

    add response row

--- showSubmissions.js
+++ showSubmissions.js
@@ -382,3 +382,6 @@ submissions[46] = submissions[46].concat('<li>Martín: ' + showcode(46,"https://
 if (submissions[46] == undefined) {submissions[46] = "";}
 submissions[46] = submissions[46].concat('<li>Marga: ' + showcode(46,"https://raw.githubusercontent.com/coding19-imdea/coding19-imdea.github.io/master/submissions/problem46/Marga.cpp", "c++") + '</li>'
 );
+if (submissions[46] == undefined) {submissions[46] = "";}
+submissions[46] = submissions[46].concat('<li>Ale: ' + showcode(46,"https://raw.githubusercontent.com/coding19-imdea/coding19-imdea.github.io/master/submissions/problem46/Ale.cpp", "c++") + '</li>'
+);

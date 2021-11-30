5815b321e276b0dd3c9c9aaebcc867f4b48a1d4a
coding19-imdea <coding19.imdea@gmail.com>
2021-11-30 10:04:40 +0100

    add response row

--- showSubmissions.js
+++ showSubmissions.js
@@ -385,3 +385,6 @@ submissions[46] = submissions[46].concat('<li>Marga: ' + showcode(46,"https://ra
 if (submissions[46] == undefined) {submissions[46] = "";}
 submissions[46] = submissions[46].concat('<li>Ale: ' + showcode(46,"https://raw.githubusercontent.com/coding19-imdea/coding19-imdea.github.io/master/submissions/problem46/Ale.cpp", "c++") + '</li>'
 );
+if (submissions[47] == undefined) {submissions[47] = "";}
+submissions[47] = submissions[47].concat('<li>Cesar: ' + showcode(47,"https://raw.githubusercontent.com/coding19-imdea/coding19-imdea.github.io/master/submissions/problem47/Cesar.ml", "ocaml") + '</li>'
+);
